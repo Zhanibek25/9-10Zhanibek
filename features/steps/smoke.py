336548ea80082d184c4f45eeac1023b6709de672
Zhanibek25 <57356515+Zhanibek25@users.noreply.github.com>
2021-11-14 15:50:49 +0600

    Update smoke.py

--- features/steps/smoke.py
+++ features/steps/smoke.py
@@ -33,42 +33,33 @@ def login(context):
     context.driver.find_element_by_id("kz.homecredit.ibank.debug:id/etInputText").send_keys("12345678")
     custom_button.click()
 
-    time.sleep(5)
+    time.sleep(2)
     try:
         context.driver.find_element_by_id("kz.homecredit.ibank.debug:id/ovOtpCode").send_keys('0000')
     except NoSuchElementException:
         print('OK: Otp not called')
 
-    #заходим в платежи
+    context.driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout[3]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[3]').click()
 
-    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Платежи")').click()
     time.sleep(3)
+    #context.driver.find_element_by_android_uiautomator('new UiSelector().text("Открыть дебетную карту")').click()
+    context.driver.find_element_by_id('kz.homecredit.ibank.debug:id/customButton').click()
+    time.sleep(2)
 
+    context.driver.find_element_by_id('kz.homecredit.ibank.debug:id/relativeLayout').send_keys("йцукен")
+    time.sleep(2)
     try:
         context.driver.find_element_by_android_uiautomator('new UiSelector().text("Алматы")').click()
     except NoSuchElementException:
         print('OK: region selection did not come out, do not have to select the region')
 
-    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Мобильная связь")').click()
-    time.sleep(3)
-    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Мобильный номер")').click()
+    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Улица")').send_keys("Al-Farabi")
     time.sleep(2)
-    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Добавить новый номер")').click()
+    context.driver.find_element_by_android_uiautomator('new UiSelector().text("№ дома")').send_keys("71")
     time.sleep(2)
-    context.driver.find_element_by_id("kz.homecredit.ibank.debug:id/phoneBookInput").click()
-    # 7758985404
-    context.driver.press_keycode(14)
-    context.driver.press_keycode(14)
-    context.driver.press_keycode(12)
-    context.driver.press_keycode(15)
-    context.driver.press_keycode(16)
-    context.driver.press_keycode(15)
-    context.driver.press_keycode(12)
-    context.driver.press_keycode(11)
-    context.driver.press_keycode(7)
-    context.driver.press_keycode(11)
+    context.driver.find_element_by_android_uiautomator('new UiSelector().text("Квартира")').send_keys("12")
+    time.sleep(2)
+    context.driver.find_element_by_id('kz.homecredit.ibank.debug:id/customButton').click()
 
 
     print('test ok')
-
-
